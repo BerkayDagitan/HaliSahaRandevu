002df633cac512e9b80c722ff4c755f96c4087fa
Berkay Dağıtan <stackzyy@gmail.com>
2025-05-06 20:45:07 +0300

    06.05.2025

---
+++ BusinessLayer/Interfaces/Tokens/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using EntityLayer.Entitys;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace BusinessLayer.Interfaces.Tokens
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
+
+
+    }
+}
